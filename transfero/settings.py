9ca91487f77eb87616424371ede52058dca488a3
Lucas Pansani <lucasinformatico066@gmail.com>
2025-03-17 16:36:25 -0300

    Enviando arquivos da aula 17/03/2025

--- transfero/settings.py
+++ transfero/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'transfero.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Tupla que informa que direciona os arquivos estaticos para dentro da pasta base_static.
+STATICFILES_DIRS = (
+    BASE_DIR / 'base_static',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
