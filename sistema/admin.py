94ab90751a0979d0457bf3660684df81beecdc45
Lucas Pansani <lucasinformatico066@gmail.com>
2025-03-28 16:31:05 -0300

    Enviando atualizações do dia 28/03

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here. 
+from sistema import models
+
+# Aqui fica o registro do usuario
+@admin.register(models.Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome','email','ativo',)
+
+#Aqui fica o registro do filme
+@admin.register(models.Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome','ano','estudio','genero',)
+
+#Aqui fica o registro do genero
+@admin.register(models.Genero)
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome',)
+
--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length = 50)
+    sobrenome = models.CharField(max_length = 50)
+    cpf = models.CharField(max_length = 20)
+    telefone = models.CharField(max_length = 20)
+    email = models.EmailField()
+    endereco = models.CharField(max_length = 100)
+    data_cadastro = models.DateTimeField(default = timezone.now)
+    ativo = models.BooleanField(default = True)
+    
+    def _str_(self):
+        return f'{self.nome} {self.sobrenome}'
+    
+
+class Filme(models.Model):
+    nome = models.CharField(max_length = 50)
+    ano = models.DateField(default = timezone.now)
+    estudio = models.CharField(max_length = 100)
+    genero = models.CharField(max_length = 70)
+    sinopse = models.CharField(max_length = 300)
+    data_cadastro = models.DateTimeField(default = timezone.now)
+    
+    def _str_(self):
+        return self.nome
+    
+
+
+
+class Genero(models.Model):
+    nome = models.CharField(max_length = 50)
+    data_cadastro = models.DateTimeField(default = timezone.now)
+     
+    def _str_(self):
+        return self.nome
