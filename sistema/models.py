8708de9976eeb3999ddc208df9b855969df0935b
Lucas Pansani <lucasinformatico066@gmail.com>
2025-03-31 16:39:55 -0300

    atualização 31/03

--- sistema/models.py
+++ sistema/models.py
@@ -10,28 +10,41 @@ class Usuario(models.Model):
     endereco = models.CharField(max_length = 100)
     data_cadastro = models.DateTimeField(default = timezone.now)
     ativo = models.BooleanField(default = True)
-    
+    imagem = models.ImageField(blank = True, upload_to='imagens/%Y/%m')
     def _str_(self):
         return f'{self.nome} {self.sobrenome}'
-    
-
-class Filme(models.Model):
-    nome = models.CharField(max_length = 50)
-    ano = models.DateField(default = timezone.now)
-    estudio = models.CharField(max_length = 100)
-    genero = models.CharField(max_length = 70)
-    sinopse = models.CharField(max_length = 300)
-    data_cadastro = models.DateTimeField(default = timezone.now)
-    
-    def _str_(self):
-        return self.nome
-    
 
 
 
 class Genero(models.Model):
     nome = models.CharField(max_length = 50)
     data_cadastro = models.DateTimeField(default = timezone.now)
-     
+    
     def _str_(self):
-        return self.nome
+        return {self.nome}
+
+
+class Filme(models.Model):  
+    nome = models.CharField(max_length = 50)
+    ano = models.DateField(default = timezone.now)
+    estudio = models.CharField(max_length = 100)
+    # genero = models.CharField(max_length = 70)
+    genero = models.ForeignKey(Genero, on_delete = models.SET_NULL, null = True, blank = True,) 
+    sinopse = models.CharField(max_length = 300)
+    data_cadastro = models.DateTimeField(default = timezone.now)
+    
+    def _str_(self):
+        return {self.nome} 
+    
+
+
+
+
+    
+
+
+
+
+#PK -> CHAVE PRIMÁRIA (PRIMARY KEY) - IDENTIFICAÇÃO ÚNICA - ID 
+#FK -> CHAVE ESTRANGEIRA (FOREIGN KEY) - LIGAÇÃO DE COLUNAS ENTRE TABELAS    
+    
