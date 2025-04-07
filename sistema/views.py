43124bd4820b0ba910d39bb627eaff077785328b
Lucas Pansani <lucasinformatico066@gmail.com>
2025-04-07 16:36:29 -0300

    07/04

--- sistema/views.py
+++ sistema/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
 # Aqui irão ficar todas as views (controladores) refrente ao sistema. 
-
+# A dunção index informa o que vai acontecer quando ela for chamada.
 def index(request):
     return render(
         request, 
-        'sistema/index.html',
+        'sistema/sistema.html',
+        'sistema/apresentacao.html',
         
     )
+
+
+
+
+# REQUEST
+# RESPONSE 
